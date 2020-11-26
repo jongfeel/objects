eb7d3945e8c26af7f2205670e900b515b441b45d
jongfeel <DeveloperFEEL@gmail.com>
2020-11-26 23:21:09 +0900

    Refactoring Ticket: remove field fee, add Fee property set, property init, remove Ticket constructor

--- Chapter01/ImprovedDesign/Theater/Ticket.cs
+++ Chapter01/ImprovedDesign/Theater/Ticket.cs
@@ -2,9 +2,5 @@ using System;
 
 public class Ticket {
     
-    private long fee;
-
-    public long Fee => fee;
-
-    public Ticket(long fee) => this.fee = fee;
+    public long Fee { get; set; } = 10000;
 }
