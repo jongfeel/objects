b2f9f0eb48fe1307199f185e041acff4af96911f
jongfeel <DeveloperFEEL@gmail.com>
2022-03-12 21:29:43 +0900

    Add design changed: add Screening.CalculateFee() method and update design document

--- Chapter04/Movie/Screening.cs
+++ Chapter04/Movie/Screening.cs
@@ -5,4 +5,22 @@ public class Screening
     public Movie Movie { set; get; }
     public int Sequence { set; get; }
     public DateTime WhenScreened { set; get; }
+
+    public Money CalculateFee(int audienceCount)
+    {
+        Money money = Movie.CalculateNoneDiscountedFee;
+        if (Movie.IsDiscountable(WhenScreened, Sequence))
+        {
+            if (Movie.MovieType == MovieType.AMOUNT_DISCOUNT)
+            {
+                money = Movie.CalculateAmountDiscountedFee;
+            }
+            else if (Movie.MovieType == MovieType.PERCENT_DISCOUNT)
+            {
+                money = Movie.CalculatePercentDiscountedFee;
+            }
+        }
+
+        return money * audienceCount;
+    }
 }
