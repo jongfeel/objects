7ed21d7e3f5019adf34690b03072b14b17eb13f0
jongfeel <DeveloperFEEL@gmail.com>
2022-03-12 19:42:08 +0900

    Refactoring DiscountCondition: add two overload methods IsDiscountable()

--- Chapter04/Movie/DiscountCondition.cs
+++ Chapter04/Movie/DiscountCondition.cs
@@ -7,4 +7,7 @@ public class DiscountCondition
     public DayOfWeek DayOfWeek { set; get; }
     public DateTime StartTime { set; get; }
     public DateTime EndTime { set; get; }
+
+    public bool IsDiscountable(DayOfWeek dayOfWeek, DateTime time) => Type == DiscountConditionType.PERIOD ? DayOfWeek == dayOfWeek && StartTime < time && EndTime > time : false;
+    public bool IsDiscountable(int sequence) => Type == DiscountConditionType.SEQUENCE ? Sequence == sequence : false;
 }
